46b6fcee3846580f36ec27bdf01360a8ba9c4264
MMOla92 <mazzieola@hotmail.com>
2022-08-22 12:59:28 +0200

    video2

--- scripts/chp2/video2/mapmaker_start.py
+++ scripts/chp2/video2/mapmaker_start.py
@@ -1,2 +1,12 @@
 class Point():
-    pass
+
+    def __init__(self, name, latitude, longitude) -> None:
+        self.name = name
+
+        if not(-90 <= latitude <=90) or not (-180 <= longitude <=180):
+            raise ValueError ("Invalid latitude, longitude combination")
+        self.longitude = longitude
+        self.latitude = latitude
+    
+    def get_lat_long(self):
+        return(self.latitude, self.longitude)
--- tests/chp2/video2/test_mapmaker_start.py
+++ tests/chp2/video2/test_mapmaker_start.py
@@ -1,4 +1,10 @@
-
+from scripts.chp2.video2.mapmaker_start import Point
 
 def test_make_one_point():
-    pass
+    p1 = Point("Dakar", 14.7167, 17.4677)
+    assert p1.get_lat_long == (14.7167, 17.4677)
+
+def test_invalid_point_gen():
+    with pytest.raises(ValueError) as exp:
+        Point("Buenos Aires", 12.11386, -555.08268)
+    assert str(exp.value) == "Invalid latitude, longitude"
