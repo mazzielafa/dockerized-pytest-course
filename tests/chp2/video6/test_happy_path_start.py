9a37cb88c9f9329f78e612cd0fcb297fa9657c66
MMOla92 <mazzieola@hotmail.com>
2022-08-22 13:41:29 +0200

    happy

--- tests/chp2/video6/test_happy_path_start.py
+++ tests/chp2/video6/test_happy_path_start.py
@@ -1,3 +1,5 @@
+from codecs import ascii_encode
+from turtle import st
 import pytest
 
 from scripts import data_processor
@@ -26,9 +28,22 @@ def test_csv_reader_data_contents(process_data):
     Happy Path Test to examine that each row
     has the appropriate data type per field
     """
-    # data = process_data
-
+    data = process_data
+    
     # Check row types
+    for row in data:
+        # operations
+        assert(isinstance(row["Country"], str))
+        assert(isinstance(row["City"], str))
+        assert(isinstance(row["State_Or_Province"], str))
+        assert(isinstance(row["Lat"], float))
+        assert(isinstance(row["Long"], float))
+        assert(isinstance(row["Altitude"], float))
+    
+    assert len(data) == 180
+    assert data[0]["Country"] == "Andorra"
+    assert data[179] ["Country"] == "United States"
+
 
     # Basic data checks
 
